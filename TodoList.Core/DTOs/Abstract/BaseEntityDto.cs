e9b126a51712f650aad14a5477fb9d968edf3dbb
AloKzMstf <mstf.gdik19@gmail.com>
2022-10-17 11:30:54 +0300

    Düzenlemeler yapıldı.

--- TodoList.Core/DTOs/Abstract/BaseEntityDto.cs
+++ TodoList.Core/DTOs/Abstract/BaseEntityDto.cs
@@ -3,7 +3,7 @@
     public abstract class BaseEntityDto
     {
         public int Id { get; set; }
-        public DateTime CreatedDate { get; set; }
-        public DateTime UpdatedDate { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
     }
 }
